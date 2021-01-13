d03612c6db03adb8cf3d8d9548ca6c9e882ad310
manishc1775 <manishchafle@gmail.com>
2021-01-13 15:31:38 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers= new [] {12.5,10.3,6.11};
+            numbers[0] = 12.5;
+            numbers[1] = 10.3;
+            numbers[2] = 6.11;
+            var result = numbers[0];       
+            result = result + numbers[1];
+            result = result + numbers[2];
+            Console.WriteLine(result);
+
+            if(args.Length > 0) 
+            {
+            Console.WriteLine($"Hello {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+
+        }
+    }
+}
